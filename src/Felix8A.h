ee8df76f886c75dbf3666b258be230eba8cf187f
Felix Ochoa <fo59paz@yahoo.com>
2026-06-15 21:19:09 -0500

    updates

--- src/Felix8A.h
+++ src/Felix8A.h
@@ -43,6 +43,15 @@ namespace Felix8A {
       Color::ROSE,          Color::CRIMSON,     Color::LILAC,
     };
 
+    static const uint32_t Spectrum36[] = {
+      Color::CORAL,     Color::MAROON,  Color::RED,     Color::VERMILION, Color::ORANGE,        Color::AMBER,
+      Color::MOCHA,     Color::OLIVE,   Color::YELLOW,  Color::LIME,      Color::CHARTREUSE,    Color::HARLEQUIN,
+      Color::HONEYDEW,  Color::EMERALD, Color::GREEN,   Color::MINT,      Color::SPRING_GREEN,  Color::TURQUOISE,
+      Color::CELESTE,   Color::TEAL,    Color::CYAN,    Color::SKY_BLUE,  Color::AZURE,         Color::SAPPHIRE,
+      Color::LAVENDER,  Color::NAVY,    Color::BLUE,    Color::INDIGO,    Color::VIOLET,        Color::ELECTRIC_PURPLE,
+      Color::LILAC,     Color::PURPLE,  Color::MAGENTA, Color::HOT_PINK,  Color::ROSE,          Color::CRIMSON,
+    };
+
     static const uint32_t Spectrum24[] = {
       Color::RED,       Color::VERMILION,   Color::ORANGE,        Color::AMBER,
       Color::YELLOW,    Color::LIME,        Color::CHARTREUSE,    Color::HARLEQUIN,
@@ -77,6 +86,7 @@ namespace Felix8A {
   static const Palette Ocean(Sets::OceanSet, FELIX8A_ARRAY_SIZE(Sets::OceanSet));
   static const Palette Blush(Sets::BlushSet, FELIX8A_ARRAY_SIZE(Sets::BlushSet));
 
+  static const Palette Palette36(Sets::Spectrum36, FELIX8A_ARRAY_SIZE(Sets::Spectrum36));
   static const Palette Palette24(Sets::Spectrum24, FELIX8A_ARRAY_SIZE(Sets::Spectrum24));
   static const Palette Palette12(Sets::Spectrum12, FELIX8A_ARRAY_SIZE(Sets::Spectrum12));
   static const Palette Palette6(Sets::Spectrum6, FELIX8A_ARRAY_SIZE(Sets::Spectrum6));
